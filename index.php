48200421115fee1f89de0ad181c6b2bca5c2a909
narimi-15772 <15772@ait.nsw.edu.au>
2025-04-30 01:02:40 +1000

    Update index.php

--- index.php
+++ index.php
@@ -12,54 +12,14 @@ include "components/head.php";
             <h2>Featured Categories</h2>
             <div class="categories">
                 <div class="card">
-                    <img class="card-image" alt="a pair of shoes" src="images/branislav-belko-lJ7iAZxplpc-unsplash.jpg">
+                    <img class="card-image" alt="a pair of shoes" src="shoes.png">
                     <div class="card-content">
                         <h4>Shoes</h4>
                         <p class="description">Sports shoes</p>
                         <a href="#" class="card-button">Shop</a>
                     </div>
                 </div>
-                <div class="card">
-                    <img class="card-image" alt="a tennis racquet and a ball"
-                        src="images/hoi-pham-eLZwsPO8cCQ-unsplash.jpg">
-                    <div class="card-content">
-                        <h4>Tennis</h4>
-                        <p class="description">Tennis gear</p>
-                        <a href="#" class="card-button">Shop</a>
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-image" src="images/ruslan-ruslan-AhAMJgq5QPM-unsplash.jpg">
-                    <div class="card-content">
-                        <h4>Basketball</h4>
-                        <p class="description">Basketball gear</p>
-                        <a href="#" class="card-button">Shop</a>
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-image" src="images/ruslan-ruslan-AhAMJgq5QPM-unsplash.jpg">
-                    <div class="card-content">
-                        <h4>Basketball</h4>
-                        <p class="description">Basketball gear</p>
-                        <a href="#" class="card-button">Shop</a>
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-image" src="images/ruslan-ruslan-AhAMJgq5QPM-unsplash.jpg">
-                    <div class="card-content">
-                        <h4>Basketball</h4>
-                        <p class="description">Basketball gear</p>
-                        <a href="#" class="card-button">Shop</a>
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-image" src="images/ruslan-ruslan-AhAMJgq5QPM-unsplash.jpg">
-                    <div class="card-content">
-                        <h4>Basketball</h4>
-                        <p class="description">Basketball gear</p>
-                        <a href="#" class="card-button">Shop</a>
-                    </div>
-                </div>
+                
             </div>
         </section>
     </main>
