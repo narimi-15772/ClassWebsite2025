61979934f830e7dd3af3a114e00dea1fa7c4e9e2
narimi-15772 <15772@ait.nsw.edu.au>
2025-04-30 00:58:45 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<?php
+// include head section
+include "components/head.php";
+?>
+
+<body>
+    <?php include "components/header.php"; ?>
+    <main>
+        <section class="featured">
+            <h2>Featured Categories</h2>
+            <div class="categories">
+                <div class="card">
+                    <img class="card-image" alt="a pair of shoes" src="images/branislav-belko-lJ7iAZxplpc-unsplash.jpg">
+                    <div class="card-content">
+                        <h4>Shoes</h4>
+                        <p class="description">Sports shoes</p>
+                        <a href="#" class="card-button">Shop</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-image" alt="a tennis racquet and a ball"
+                        src="images/hoi-pham-eLZwsPO8cCQ-unsplash.jpg">
+                    <div class="card-content">
+                        <h4>Tennis</h4>
+                        <p class="description">Tennis gear</p>
+                        <a href="#" class="card-button">Shop</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-image" src="images/ruslan-ruslan-AhAMJgq5QPM-unsplash.jpg">
+                    <div class="card-content">
+                        <h4>Basketball</h4>
+                        <p class="description">Basketball gear</p>
+                        <a href="#" class="card-button">Shop</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-image" src="images/ruslan-ruslan-AhAMJgq5QPM-unsplash.jpg">
+                    <div class="card-content">
+                        <h4>Basketball</h4>
+                        <p class="description">Basketball gear</p>
+                        <a href="#" class="card-button">Shop</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-image" src="images/ruslan-ruslan-AhAMJgq5QPM-unsplash.jpg">
+                    <div class="card-content">
+                        <h4>Basketball</h4>
+                        <p class="description">Basketball gear</p>
+                        <a href="#" class="card-button">Shop</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-image" src="images/ruslan-ruslan-AhAMJgq5QPM-unsplash.jpg">
+                    <div class="card-content">
+                        <h4>Basketball</h4>
+                        <p class="description">Basketball gear</p>
+                        <a href="#" class="card-button">Shop</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include "components/footer.php"; ?>
+</body>
+
+</html>
